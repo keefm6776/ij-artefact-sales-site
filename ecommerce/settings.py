e94a527212b647091a82097270902277a3822712
keefm6776 <keefm_6776@yahoo.com>
2019-10-28 18:30:38 +0000

    added app to allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'ijones-artefact-sales.herokuapp.com']
 
 
 # Application definition
