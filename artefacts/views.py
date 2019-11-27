6ad538c7003c4c9378ae4a0d284287d4bf927b14
keefm6776 <keefm_6776@yahoo.com>
2019-11-27 01:04:32 +0000

    Add Pagination to artefact pages

--- artefacts/views.py
+++ artefacts/views.py
@@ -7,26 +7,67 @@ from artefacts.models import Artefact
 from checkout.models import OrderLineItem, Order
 from .forms import ArtefactForm
 from weasyprint import HTML
-    
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 def for_sale_artefacts(request):
     """ Finds all unsold artefacts in the database and displays them """
     artefacts = Artefact.objects.filter(sold=False)
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(artefacts, 10)
+
+    try:
+        artefacts = paginator.page(page)
+    except PageNotAnInteger:
+        artefacts = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
     return render(request, "artefacts.html", {"artefacts": artefacts})
+
 
 def sold_artefacts(request):
     """ Finds all sold artefacts in the database and displays them """
     artefacts = Artefact.objects.filter(sold=True, despatched=False)
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(artefacts, 10)
+
+    try:
+        artefacts = paginator.page(page)
+    except PageNotAnInteger:
+        artefacts = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
     return render(request, "artefacts_sold.html", {"artefacts": artefacts})
+
 
 def despatched_artefacts(request):
     """ Finds all sold artefacts in the database and displays them """
     artefacts = Artefact.objects.filter(despatched=True)
+
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(artefacts, 10)
+
+    try:
+        artefacts = paginator.page(page)
+    except PageNotAnInteger:
+        artefacts = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
     return render(request, "artefacts_despatched.html", {"artefacts": artefacts})
+
 
 def artefact_detail(request, pk):
     """ Displays all the artefact details to the user """
     artefact = get_object_or_404(Artefact, pk=pk)
     return render(request, "artefact_detail.html", {"artefact": artefact})
+
 
 def add_artefact(request):
     """ Allows Site Owner To Add New Artefact To Sell """
@@ -42,7 +83,8 @@ def add_artefact(request):
             form = ArtefactForm()
             return redirect(for_sale_artefacts)
     return render(request, "add_artefact.html", {'form': form})
-          
+
+
 def edit_artefact_detail(request, id):
     """ Displays the selected Artefact to the Site Owner for editing """
     artefact = get_object_or_404(Artefact, pk=id)
@@ -55,16 +97,17 @@ def edit_artefact_detail(request, id):
                 form.save()
                 return redirect(for_sale_artefacts)
             else:
-                form.add_error(None, "The Century Cannot be negative or zero!")          
+                form.add_error(None, "The Century Cannot be negative or zero!")
     else:
         form = ArtefactForm(instance=artefact)
 
     return render(request, "edit_artefact_detail.html", {'form': form})
 
+
 def despatch_artefact(request, id):
     """ Marks the artefact as despatched, with despatched date """
     """ Sending html to a pdf file adapted from code at Simpleisbetterthancomplex.com"""
-    
+
     artefact = get_object_or_404(Artefact, id=id)
     artefact.despatched = True
     artefact.despatch_date = timezone.now()
@@ -73,21 +116,24 @@ def despatch_artefact(request, id):
     order_line_info = OrderLineItem.objects.filter(artefact__pk=id)
     order_id = Order.objects.filter(orderlineitem=order_line_info)
     delivery = get_object_or_404(Order, pk=order_id)
-                    
-    artefact_info = {"name": artefact.name, "price": artefact.price, "order": delivery}
-    html_string = render_to_string('../templates/despatch_template.html', {'artefact_info': artefact_info})
+
+    artefact_info = {"name": artefact.name,
+                     "price": artefact.price, "order": delivery}
+    html_string = render_to_string(
+        '../templates/despatch_template.html', {'artefact_info': artefact_info})
 
     html = HTML(string=html_string)
-    html.write_pdf(target='/tmp/despatch note.pdf');
+    html.write_pdf(target='/tmp/despatch note.pdf')
 
     fs = FileSystemStorage('/tmp')
     with fs.open('despatch note.pdf') as pdf:
         response = HttpResponse(pdf, content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="despatch note.pdf"'
         return response
-      
+
     artefacts = Artefact.objects.filter(despatched=True)
     return render(request, "artefacts_despatched.html", {"artefacts": artefacts})
+
 
 def delete_artefact(request, id):
     """ Allows the Site Owner to delete the current artefact """
