fad5cb0c8c374db30313759c39dc3bee7e1d6c10
keefm6776 <keefm_6776@yahoo.com>
2020-01-03 23:59:25 +0000

    reintroduce required fields in customer and artefact

--- artefacts/models.py
+++ artefacts/models.py
@@ -5,18 +5,18 @@ from ecommerce import settings
 
 
 class Artefact(models.Model):
-    name = models.CharField(max_length=254, default='', blank=True)
-    history = models.TextField(blank=True)
-    description = models.TextField(blank=True)
-    century = models.DecimalField(max_digits=5, decimal_places=0, blank=True)
+    name = models.CharField(max_length=254, default='', blank=False)
+    history = models.TextField(blank=False)
+    description = models.TextField(blank=False)
+    century = models.DecimalField(max_digits=5, decimal_places=0, blank=False)
 
     ERA_CHOICES = (
         ('AD', 'Anno Domini'),
         ('BC', 'Before Christ'),
     )
-    era = models.CharField(max_length=2, choices=ERA_CHOICES, blank=True)
+    era = models.CharField(max_length=2, choices=ERA_CHOICES, blank=False)
     image = models.ImageField(upload_to='static', blank=True)
-    price = models.DecimalField(max_digits=9, decimal_places=2, blank=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, blank=False)
     sold = models.BooleanField(default=False)
     despatched = models.BooleanField(default=False)
     despatch_date = models.DateField(null=True, blank=True)
--- customer/models.py
+++ customer/models.py
@@ -5,15 +5,15 @@ from django.dispatch import receiver
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=60, blank=True)
-    street_Address1 = models.CharField(max_length=40, blank=True)
-    street_Address2 = models.CharField(max_length=40, blank=True)
-    town_or_city = models.CharField(max_length=40, blank=True)
-    county = models.CharField(max_length=40, blank=True)
-    country = models.CharField(max_length=40, blank=True)
-    postcode = models.CharField(max_length=20, blank=True)
-    phone_number = models.CharField(max_length=20, blank=True)
-    email = models.CharField(max_length=254, blank=True)
+    full_name = models.CharField(max_length=60, blank=False)
+    street_Address1 = models.CharField(max_length=40, blank=False)
+    street_Address2 = models.CharField(max_length=40, blank=False)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    email = models.CharField(max_length=254, blank=False)
     
     def __str__(self):
         return str(self.id)
