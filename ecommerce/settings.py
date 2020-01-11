97d28ee8a7612f502ac932237a1ab38444afd4e3
keefm6776 <keefm_6776@yahoo.com>
2020-01-11 00:59:31 +0000

    recomment out DATABASE heading causing error

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import env
+import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -86,8 +86,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 #    }
 
 
-Database
-https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+#Database
+#https://docs.djangoproject.com/en/1.11/ref/settings/databases
 
 if "DATABASE_URL" in os.environ:
    DATABASES = {'default': dj_database_url.parse(
